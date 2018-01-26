5742ddd7765dab92b2ee42b34ce3d9b6e8b996de
Administrator <1135221105@qq.com>
2018-01-26 16:25:39 +0800

    ti jiao

--- app/src/main/java/com/example/customwidget/CustomActivity.java
+++ app/src/main/java/com/example/customwidget/CustomActivity.java
@@ -1,20 +1,14 @@
 package com.example.customwidget;
 
-import android.animation.ObjectAnimator;
-
 import com.example.base.BaseActivity;
 import com.example.base.R;
-import com.example.customwidget.widget.Custom01View;
-import com.example.customwidget.widget.obj_point.MyAnimView;
-
-import butterknife.BindView;
 
 public class CustomActivity extends BaseActivity {
 
-    @BindView(R.id.custom01view)
-    Custom01View custom01view;
-    @BindView(R.id.anim_view)
-    MyAnimView animView;
+//    @BindView(R.id.custom01view)
+//    Custom01View custom01view;
+//    @BindView(R.id.anim_view)
+//    MyAnimView animView;
 
 
     @Override
@@ -24,12 +18,7 @@ public class CustomActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        ObjectAnimator animator1 = ObjectAnimator.ofFloat(custom01view, "Alpha", 1, 0, 1);
-        animator1.setDuration(3000);
-        animator1.start();
-        ObjectAnimator animator2 = ObjectAnimator.ofFloat(custom01view, "TranslationX", 100, 0);
-        animator2.setDuration(3000);
-        animator2.start();
+
         //
     }
 
