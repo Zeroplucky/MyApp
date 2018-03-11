2dcb0b50169a8c22e3536e063c45e0ff2dee139c
ZhangPeng <1135221105@qq.com>
2018-03-11 19:49:24 +0800

    ti jiao

--- app/src/main/java/com/example/customwidget/CustomActivity.java
+++ app/src/main/java/com/example/customwidget/CustomActivity.java
@@ -14,8 +14,7 @@ import butterknife.BindView;
 public class CustomActivity extends BaseActivity {
 
 
-    @BindView(R.id.xxx)
-    CalendarView xxx;
+
 
     @Override
     protected int getContentViewId() {
@@ -23,19 +22,6 @@ public class CustomActivity extends BaseActivity {
     }
 
 
-    public void clickToChang(View view) {
-        Log.e(TAG, "clickToChang: " + xxx.getLeft() + "    " + xxx.getTranslationX() + " " + xxx.getScaleX());
-        xxx.animate()
-                .setDuration(1000)
-                .xBy(50)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        super.onAnimationEnd(animation);
-                        Log.e(TAG, "clickToChang: " + xxx.getLeft() + "    " + xxx.getTranslationX() + " " + xxx.getScaleX());
-                    }
-                }).start();
-    }
 
     @Override
     protected void initView() {
--- app/src/main/java/com/example/customwidget/widget/MyViewDragHelper.java
+++ app/src/main/java/com/example/customwidget/widget/MyViewDragHelper.java
@@ -3,6 +3,7 @@ package com.example.customwidget.widget;
 import android.content.Context;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -14,6 +15,9 @@ import android.widget.RelativeLayout;
 public class MyViewDragHelper extends RelativeLayout {
 
     private ViewDragHelper dragHelper;
+    private View leftView;
+    private View rightView;
+    private String TAG = "xxx";
 
     public MyViewDragHelper(Context context) {
         super(context);
@@ -36,20 +40,61 @@ public class MyViewDragHelper extends RelativeLayout {
 
     private class ViewCallback extends ViewDragHelper.Callback {
 
+
+        //1
         @Override
         public boolean tryCaptureView(View child, int pointerId) {
-            return false;
+            return child == rightView;
         }
+
+        //2
+        @Override
+        public void onViewCaptured(View capturedChild, int activePointerId) {
+            super.onViewCaptured(capturedChild, activePointerId);
+
+        }
+
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            Log.e(TAG, "clampViewPositionHorizontal: " + left + "  " + dx);
+            return left;
+        }
+
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            super.onViewPositionChanged(changedView, left, top, dx, dy);
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+        }
+
+
     }
 
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return super.onInterceptTouchEvent(ev);
+        return dragHelper.shouldInterceptTouchEvent(ev);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        try {
+            dragHelper.processTouchEvent(event);
+        } catch (Exception e) {
+
+        }
+        return true; //消费事件
+    }
+
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        leftView = getChildAt(0);
+        rightView = getChildAt(1);
     }
 }
