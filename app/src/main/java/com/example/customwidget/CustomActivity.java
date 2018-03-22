62fc80235541ed737ad0d3e7d72b47bd683e6da7
Administrator <1135221105@qq.com>
2018-03-22 17:39:31 +0800

    ti jiao

--- app/src/main/java/com/example/customwidget/CustomActivity.java
+++ app/src/main/java/com/example/customwidget/CustomActivity.java
@@ -1,13 +1,5 @@
 package com.example.customwidget;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.support.v4.view.PagerAdapter;
-import android.util.TypedValue;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import com.example.base.BaseActivity;
 import com.example.base.R;
 import com.example.customwidget.widget.Custom01View;
@@ -31,49 +23,6 @@ public class CustomActivity extends BaseActivity {
     @Override
     protected void initView() {
 
-//        myDragLayout.setAdapter(new PageAdaper(this));  terminate
-
-        TextView textView = new TextView(this);
-        textView.setLayoutParams(new ViewGroup.LayoutParams(-1, -1));
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-        textView.setText("xxxxxxxxxxx");
-        textView.setTextColor(Color.RED);
-
-        addContentView(textView, new ViewGroup.LayoutParams(-1, -1));
-    }
-
-    class PageAdaper extends PagerAdapter {
-
-        private Context context;
-
-        public PageAdaper(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        public int getCount() {
-            return 10;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            TextView textView = new TextView(context);
-            textView.setLayoutParams(new ViewGroup.LayoutParams(-1, -1));
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-            textView.setText("xxxxxxxxxxx");
-            textView.setTextColor(Color.RED);
-            return textView;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-
-        }
     }
 
 
