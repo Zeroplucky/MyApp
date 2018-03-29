8f5cbaff27c70784ec624ecf26eea3ba70bf6cb4
Administrator <1135221105@qq.com>
2018-03-29 16:45:11 +0800

    ti jiao

--- app/src/main/java/com/example/customwidget/adapter/MyPagerAdapter.java
+++ app/src/main/java/com/example/customwidget/adapter/MyPagerAdapter.java
@@ -3,7 +3,6 @@ package com.example.customwidget.adapter;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -60,33 +59,35 @@ public class MyPagerAdapter extends PagerAdapter {
 
 
     public static class RotationPageTransformer implements ViewPager.PageTransformer {
-        private static final float MIN_SCALE = 0.85f;
+        private static final float DEFAULT_MIN_SCALE = 0.85f;
+        private float mMinScale = DEFAULT_MIN_SCALE;
+        public static final float DEFAULT_CENTER = 0.5f;
 
         @Override
-        public void transformPage(View page, float position) {
-            page.setScaleX(0.9f);
-            Log.e("CustomActivity xxx", "transformPage: ---------------------------------------");
-            Log.e("CustomActivity xxx", "transformPage: " + page.toString() + "    " + position);
-            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-            float rotate = 10 * Math.abs(position);
-            //position小于等于1的时候，代表page已经位于中心item的最左边，
-            //此时设置为最小的缩放率以及最大的旋转度数
-            if (position <= -1) {
-                page.setScaleX(MIN_SCALE);
-                page.setScaleY(MIN_SCALE);
-                page.setRotationY(rotate);
-            } else if (position < 0) {//position从0变化到-1，page逐渐向左滑动 // [-1,0]
-                page.setScaleX(scaleFactor);
-                page.setScaleY(scaleFactor);
-                page.setRotationY(rotate);
-            } else if (position >= 0 && position < 1) {//position从0变化到1，page逐渐向右滑动  // (0,1]
-                page.setScaleX(scaleFactor);
-                page.setScaleY(scaleFactor);
-                page.setRotationY(-rotate);
-            } else if (position >= 1) {//position大于等于1的时候，代表page已经位于中心item的最右边
-                page.setScaleX(scaleFactor);
-                page.setScaleY(scaleFactor);
-                page.setRotationY(-rotate);
+        public void transformPage(View view, float position) {
+
+            int pageWidth = view.getWidth();
+            view.setPivotX(pageWidth / 2);
+            if (position < -1) { // [-Infinity,-1)
+                view.setScaleX(mMinScale);
+                view.setScaleY(mMinScale);
+                view.setPivotX(pageWidth);
+            } else if (position <= 1) { // [-1,1]
+                if (position < 0) {//1-2:1[0,-1] ;2-1:1[-1,0]
+                    float scaleFactor = (1 + position) * (1 - mMinScale) + mMinScale;
+                    view.setScaleX(scaleFactor);
+                    view.setScaleY(scaleFactor);
+                    view.setPivotX(pageWidth * (DEFAULT_CENTER + (DEFAULT_CENTER * -position)));
+                } else {//1-2:2[1,0] ;2-1:2[0,1]
+                    float scaleFactor = (1 - position) * (1 - mMinScale) + mMinScale;
+                    view.setScaleX(scaleFactor);
+                    view.setScaleY(scaleFactor);
+                    view.setPivotX(pageWidth * ((1 - position) * DEFAULT_CENTER));
+                }
+            } else { // (1,+Infinity]
+                view.setPivotX(0);
+                view.setScaleX(mMinScale);
+                view.setScaleY(mMinScale);
             }
         }
     }
